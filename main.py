417c9f1bd53edab8e5f98589a21eac1bcdbeb65a
VicentRM <vribes@odec.es>
2026-03-23 20:34:23 +0100

    Implementada persistencia

--- main.py
+++ main.py
@@ -12,25 +12,26 @@ def main():
     manager = TaskManager()
     while True:
         print_menu()
-
-        choice = input("Seleccione una opción: ")
-        match choice:
-            case "1":
-                description = input("Descripción de la tarea: ")
-                manager.add_task(description)
-            case "2":
-                manager.list_tasks()
-            case "3":
-                id = input("ID de la tarea: ")
-                manager.complete_task(id)
-            case "4":
-                id = input("ID de la tarea: ")
-                manager.delete_task(id)
-            case "5":
-                print("Saliendo...")
-                break
-            case _:
-                print("Opción no válida. Por favor, seleccione una opción válida.")
-
+        try:       
+            choice = int(input("Seleccione una opción: "))
+            match choice:
+                case 1:
+                    description = input("Descripción de la tarea: ")
+                    manager.add_task(description)
+                case 2:
+                    manager.list_tasks()
+                case 3:
+                    id = int(input("ID de la tarea: "))
+                    manager.complete_task(id)
+                case 4:
+                    id = int(input("ID de la tarea: "))
+                    manager.delete_task(id)
+                case 5:
+                    print("Saliendo...")
+                    break
+                case _:
+                    print("Opción no válida. Por favor, seleccione una opción válida.")
+        except ValueError:
+            print("Opción no válida. Por favor, seleccione una opción válida.")
 if __name__ == "__main__":
     main()
